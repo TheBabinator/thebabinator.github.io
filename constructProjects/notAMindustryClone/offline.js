cd95010ee5e42512d43b9bac97d995d3d32d0ecd
TheBabinator <39492647+TheBabinator@users.noreply.github.com>
2018-10-06 15:12:37 +0100

    Add files via upload

---
+++ constructProjects/notAMindustryClone/offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1538835051,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/grass.png",
+		"images/stone.png",
+		"images/player-sheet0.png",
+		"images/playerindicator-sheet0.png",
+		"images/playerbullet-sheet0.png",
+		"images/stoneblock.png",
+		"images/tiledbackground.png",
+		"images/playerspawn-sheet0.png",
+		"images/smallimpact-sheet0.png",
+		"images/bigimpact-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/core-sheet0.png",
+		"media/game.ogg",
+		"media/menu.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
